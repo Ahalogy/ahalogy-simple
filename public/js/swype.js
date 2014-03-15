a26c922ec9331b930e4291c1bac5b377a0bb0eaf
Scott Kacyn <scott.kacyn@gmail.com>
2014-03-14 22:47:33 -0500

    Added JQuery Mobile

---
+++ public/js/swype.js
@@ -0,0 +1,15 @@
+///////////////////////////////////
+// Global variables and settings //
+///////////////////////////////////
+$.mobile.loadingMessage = false;
+var isAnimatingScroll = false;
+
+$(document).ready(function() {
+
+  // Swipe actions
+  $(window).on("swipe",function(e) {
+  });
+
+
+});
+
