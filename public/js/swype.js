c56e3799c8c31995de643030d4ddd4f8a8ca4da1
Scott Kacyn <scott.kacyn@gmail.com>
2014-03-17 22:12:04 -0500

    -A

--- public/js/swype.js
+++ public/js/swype.js
@@ -22,8 +22,7 @@ $(document).ready(function() {
      * initial
      */
     this.init = function() {
-      setLayerDimensions();
-      $(window).on("load resize orientationchange", function() {
+      $(window).on("load orientationchange", function() {
         setLayerDimensions();
       })
     };
@@ -32,26 +31,50 @@ $(document).ready(function() {
      * set the layer dimensions and scale the container
      */
     function setLayerDimensions() {
+      panes.each(function(index) {
+
+        // This is the first data- attribute, always set
+        // to top: 0px; so that the div doesn't move before
+        // it is scrolled to
+        var prevHeight = 0;
+        for (var i = 0; i < index; i++) {
+          prevHeight += $(panes[i]).height();
+        }
+
+        // Calculate the final height which is previous height
+        // plus current div height, where we would expect the
+        // div to "go away" and have top = -(div height)
+        var currHeight = $(this).height();
+        var finalHeight = prevHeight + currHeight;
+
+        // Set the data attributes
+        $(this).attr("data-"+prevHeight,  "top:0px;display:block;");
+        $(this).attr("data-"+finalHeight, "top:-"+currHeight+"px;display:none;");
+
+        // Refresh Skrollr instance
+        s.refresh();
+      });
+
       pane_height = element.height();
-      console.log("PANE height", pane_height);
       container.height(pane_height);
     };
 
     this.showPane = function(index, animate) {
       index = Math.max(0, Math.min(index, pane_count-1));
       current_pane = index;
-
-      var offset = -((100/pane_count)*current_pane);
-      setSkrollrOffset(offset, animate);
+      setSkrollrOffset();
     };
 
-    function setSkrollrOffset(percent, animate) {
+    this.nearbySection = function(anchor) {
+      s.animateTo(anchor, {
+        duration: 200,
+        easing: 'sqrt'
+      });
+    }
+
+    function setSkrollrOffset() {
 
       var currPane = panes[current_pane];
-      var currPaneHeight = $(currPane).height();
-      var currTop = s.getScrollTop();
-      console.log("PANE HEIGHT, PANE TOP", currPaneHeight, currTop);
-
       var elapsedHeight = 0;
       for (var i = 0; i < current_pane; i++) {
         var ht = $(panes[i]).height();
@@ -59,7 +82,7 @@ $(document).ready(function() {
       }
 
       s.animateTo(elapsedHeight, {
-        duration: 400,
+        duration: 200,
         easing: 'sqrt'
       });
     }
@@ -69,54 +92,46 @@ $(document).ready(function() {
 
     function handleHammer(e) {
 
+      var prevHeight = 0;
+      for (var i = 0; i < current_pane; i++) {
+        prevHeight += $(panes[i]).height();
+      }
+
       var g = e.gesture;
       var d = s.getScrollTop();
+      var r = (d - prevHeight);
 
       g.preventDefault();
 
-      switch (e.type) {
-        case 'dragdown':
-        case 'dragup':
-          var layer_offset = -(100/pane_count)*current_pane;
-          console.log("Layer offset / current pane", layer_offset, current_pane);
-          var drag_offset = ((100/pane_height)*g.deltaY) / pane_count;
+      var divHeight = $(panes[current_pane]).height();
+      var modDivHeight = (divHeight % pane_height);
 
-          /*if ((current_pane == 0 && g.direction == "down") ||
-            (current_pane == pane_count-1 && g.direction == "up")) {
-            drag_offset *= .4;
-          }
-
-          setSkrollrOffset(drag_offset + layer_offset);*/
-          break;
-
-        case 'swipeup':
-          console.log('swipe up');
+      if (modDivHeight == 0) {
+        // The div is shorter than the viewport
+        if (g.direction == 'up') {
           self.next();
-          g.stopDetect();
-          break;
-
-        case 'swipedown':
-          console.log('swipe down');
+        } else {
           self.prev();
-          g.stopDetect();
-          break;
-
-        case 'release':
-          console.log('release');
-          if (Math.abs(g.deltaY) > pane_height/3) {
-            if (g.direction == 'up') {
-              self.next();
-            } else {
-              self.prev();
-            }
+        }
+      } else {
+        // The div is larger than the viewport
+        if (r/divHeight < .5) {
+          if (g.direction == 'up') {
+            self.nearbySection(prevHeight + modDivHeight);
           } else {
-            self.showPane(current_pane, true);
+            self.prev();
           }
-          break;
+        } else {
+          if (g.direction == 'up') {
+            self.next();
+          } else {
+            self.prev();
+          }
+        }
       }
     }
 
-    new Hammer(container[0], { drag_lock_to_axis: true }).on("release dragup dragdown slideup slidedown", handleHammer);
+    new Hammer(container[0], { drag_lock_to_axis: true }).on("release", handleHammer);
   }
 
   var stickySwipe = new StickySwipe("#stickySwipe");
