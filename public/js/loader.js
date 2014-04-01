078e6cb583951f84f8f8b175e9f9dfa429009660
Scott Kacyn <scott.kacyn@gmail.com>
2014-03-31 19:58:43 -0500

    Dynamic loading of carded content

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,58 +1,78 @@
 $(document).ready(function() {
 
-  var viewport = document.querySelector("meta[name=viewport]");
-  if (viewport) {
-    viewport.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1');
-  } else {
-    $("head").append('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">');
-  }
+  // Set appropriate meta viewport tag
+  setOrUpdateViewport();
 
   // Global Variables
   var ht = $(window).height();
 
+  // Var to store whether or not to load carded content
+  var displayCardedContent = (function()
+  {
+    var shouldDisplayCardedContent = true;
+    var pageURL = window.location.search.substring(1);
+    var URLVariables = pageURL.split('&');
+    for (var i = 0; i < URLVariables.length; i++)
+    {
+        var parameterName = URLVariables[i].split('=');
+        if (parameterName[0] == 'disableMobilify' && parameterName[1] == 1)
+        {
+            shouldDisplayCardedContent = false;
+            break;
+        }
+    }
+    return shouldDisplayCardedContent;
+  })();
+
+  // Display carded content
+  if (displayCardedContent) displayCards();
+});
+
+function displayCards() {
+
+  // Add the required stylesheet(s) and script(s)
   $("head").append("<link rel='stylesheet' href='styles/app.css'/>").
             append("<link rel='stylesheet' href='styles/animations.css'/>").
             append("<script src='js/skrollr.min.js'></script>").
             append("<script src='js/hammer.js'></script>").
             append("<script src='js/app.js'></script>");
 
-  // Var to store whether or not to load carded content
-  var displayCardedContent = (function()
-  {
-
-    // TODO:
-    // 1) Detect if we're inside of the Pinterest app
-    // 2) Detect if the referral is from Pinterest
-    // 3) Don't load on presence of disableMobilify (KVP)
-    var shouldDisplayCardedContent = true;
-    return shouldDisplayCardedContent;
-  })();
-
-  if (displayCardedContent) {
-
-    // Empty the body and replace with Loading screen
-    $("body").empty().append("<div class='loader'><div class='loading'></div><div id='loading-text'>loading</div></div>");
+  // Empty the body and replace with Loading screen
+  $("body").empty().append("<div class='loader'><div class='loading'></div><div id='loading-text'>loading</div></div>");
 
   // Attempt to fetch carded content
   // data-mobilify=1, data-clientId=[Client Identifier], data-articleId=[Article Identifier]
   // (Client ID)-(Article ID)-iphone.html
-  var request = $.ajax( "carded-content-1.html" )
-    .done(function( html ) {
+  var cardedData = $("#mobilify-loader").data();
+  var clientIdentifier = cardedData.client;
+  var articleIdentifier = cardedData.article;
+  var cardedURL = clientIdentifier + "-" + articleIdentifier + "-iphone.html";
+  var request = $.ajax( cardedURL )
+  .done(function( html ) {
+    $("body").empty().append(html).append("<div class='loader'><div class='loading'></div><div id='loading-text'>loading</div></div>");
+    // Setup Skrollr deferred
+    setTimeout(initSkrollr, 4000);
+  })
+  .fail(function() {
+    displayOriginalSite();
+  })
+  .always(function() {
+  });
+}
 
-      $("body").empty().append(html).append("<div class='loader'><div class='loading'></div><div id='loading-text'>loading</div></div>");
-      // Setup Skrollr deferred
-      setTimeout(initSkrollr, 4000);
-    })
-    .fail(function() {
-      // TODO:
-      // Redirect to the same page
-      // with a flag to not load carded contetn
-    })
-    .always(function() {
-    });
+function displayOriginalSite() {
+  var originalSiteURL = document.URL + "?disableMobilify=1";
+  window.location.href = originalSiteURL;
+}
+
+function setOrUpdateViewport() {
+  var viewport = document.querySelector("meta[name=viewport]");
+  if (viewport) {
+    viewport.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1');
   } else {
+    $("head").append('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">');
   }
-});
+}
 
 function initSkrollr() {
   console.log("Init Skrollr");
